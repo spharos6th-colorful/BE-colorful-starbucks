130c38336ac2d7d6c8ed89342d7535270322c494
yryryr96 <s9285s@naver.com>
2025-03-20 13:45:32 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/colorful/starbucks/StarbucksColorfulApplication.java
@@ -0,0 +1,13 @@
+package colorful.starbucks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarbucksColorfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarbucksColorfulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/colorful/starbucks/StarbucksColorfulApplicationTests.java
@@ -0,0 +1,13 @@
+package colorful.starbucks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarbucksColorfulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
