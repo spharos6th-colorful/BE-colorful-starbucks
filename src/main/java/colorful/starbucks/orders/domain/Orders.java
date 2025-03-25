b6ef9fae0adc34d4f0fa3e810c919e21c9721ae0
woneveryday <woneveryday@naver.com>
2025-03-25 16:05:43 +0900

    :sparkles: feat: 주문 테이블 entity

---
+++ src/main/java/colorful/starbucks/orders/domain/Orders.java
@@ -0,0 +1,67 @@
+package colorful.starbucks.orders.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Orders {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    private String orderUuid;
+
+    private Long couponId;
+
+    private int beforeDiscountPrice;
+
+    private int afterDiscountPrice;
+
+    private String memberUuid;
+
+    private String postNumber;
+
+    private String mainAddress;
+
+    private String subAddress;
+
+    private boolean payStatus;
+
+    private boolean giftStatus;
+
+    @Builder
+    private Orders(
+            Long id,
+            String orderUuid,
+            Long couponId,
+            int beforeDiscountPrice,
+            int afterDiscountPrice,
+            String memberUuid,
+            String postNumber,
+            String mainAddress,
+            String subAddress,
+            Boolean payStatus,
+            Boolean giftStatus
+
+    ){
+        this.id = id;
+        this.orderUuid = orderUuid;
+        this.couponId = couponId;
+        this.beforeDiscountPrice = beforeDiscountPrice;
+        this.afterDiscountPrice = afterDiscountPrice;
+        this.memberUuid = memberUuid;
+        this.postNumber = postNumber;
+        this.mainAddress = mainAddress;
+        this.subAddress = subAddress;
+        this.payStatus = payStatus;
+        this.giftStatus = giftStatus;
+
+    }
+}
