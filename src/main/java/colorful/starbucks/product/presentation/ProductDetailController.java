3caa044cc349d1c0da4548b7a04eb08d71ce323f
yryryr96 <s9285s@naver.com>
2025-03-26 19:09:37 +0900

    :sparkles:feat: ProductDetail 컨트롤러 생성

---
+++ src/main/java/colorful/starbucks/product/presentation/ProductDetailController.java
@@ -0,0 +1,31 @@
+package colorful.starbucks.product.presentation;
+
+import colorful.starbucks.common.response.ApiResponse;
+import colorful.starbucks.product.application.ProductDetailService;
+import colorful.starbucks.product.dto.request.ProductDetailCreateRequestDto;
+import colorful.starbucks.product.vo.ProductDetailCreateRequestVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class ProductDetailController {
+
+    private final ProductDetailService productDetailService;
+
+    @PostMapping("/products/{productCode}/details")
+    public ApiResponse<Void> createProductDetail(
+            @PathVariable("productCode") String productCode,
+            @RequestPart ProductDetailCreateRequestVo request,
+            @RequestPart MultipartFile productDetailThumbnail) {
+
+        System.out.println("productCode = " + productCode);
+        productDetailService.createProductDetail(productCode,
+                ProductDetailCreateRequestDto.from(request),
+                productDetailThumbnail);
+
+        return ApiResponse.ok("상세 상품이 등록되었습니다.", null);
+    }
+}
