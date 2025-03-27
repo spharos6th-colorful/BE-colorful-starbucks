a1e9fd637e6ecf175e4e26fd8b412246de03cff2
yryryr96 <s9285s@naver.com>
2025-03-27 17:47:16 +0900

    :hammer:refactor: InterestProductCreate 메서드를 InterestProductController로 책임 이동

--- src/main/java/colorful/starbucks/product/presentation/ProductController.java
+++ src/main/java/colorful/starbucks/product/presentation/ProductController.java
@@ -1,17 +1,16 @@
 package colorful.starbucks.product.presentation;
 
 import colorful.starbucks.common.response.ApiResponse;
-import colorful.starbucks.product.application.InterestProductService;
 import colorful.starbucks.product.application.ProductService;
-import colorful.starbucks.product.dto.request.InterestProductCreateRequestDto;
 import colorful.starbucks.product.dto.request.ProductCreateRequestDto;
-import colorful.starbucks.product.vo.request.InterestProductCreateRequestVo;
 import colorful.starbucks.product.vo.request.ProductCreateRequestVo;
-import colorful.starbucks.product.vo.response.InterestProductCreateResponseVo;
 import colorful.starbucks.product.vo.response.ProductCreateResponseVo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -20,7 +19,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class ProductController {
 
     private final ProductService productService;
-    private final InterestProductService interestProductService;
 
     @PostMapping
     public ApiResponse<ProductCreateResponseVo> createProduct(@RequestPart ProductCreateRequestVo productCreateRequestVo,
@@ -31,21 +29,6 @@ public class ProductController {
                 "상품 등록을 완료했습니다." ,
                 productService.create(
                         ProductCreateRequestDto.from(productCreateRequestVo), productThumbnail, productCommonImage)
-                        .toVo()
-        );
-    }
-
-    // security 완료되면 memberUuid는 authentication에서 가져오도록 수정
-    @PostMapping("/interest/{memberUuid}")
-    public ApiResponse<InterestProductCreateResponseVo> createInterestProduct(
-            @RequestBody InterestProductCreateRequestVo interestProductCreateRequestVo,
-            @PathVariable String memberUuid) {
-
-        return ApiResponse.of(
-                HttpStatus.CREATED,
-                "관심 상품 등록을 완료했습니다." ,
-                interestProductService.createInterestProduct(
-                        InterestProductCreateRequestDto.from(interestProductCreateRequestVo, memberUuid))
                         .toVo()
         );
     }
