7a50741b30f36d590d43912670f93a8f3eb6610d
정영록 <s9285s@naver.com>
2025-03-27 21:39:49 +0900

    :hammer:refactor: 관심상품 생성 응답 dto 필드에 productName 추가

--- src/main/java/colorful/starbucks/product/dto/response/InterestProductCreateResponseDto.java
+++ src/main/java/colorful/starbucks/product/dto/response/InterestProductCreateResponseDto.java
@@ -7,32 +7,37 @@ import lombok.Builder;
 public class InterestProductCreateResponseDto {
 
     private String productCode;
+    private String productName;
     private int price;
     private String productThumbnailUrl;
 
     @Builder
     private InterestProductCreateResponseDto(
             String productCode,
+            String productName,
             int price,
             String productThumbnailUrl) {
         this.productCode = productCode;
+        this.productName = productName;
         this.price = price;
         this.productThumbnailUrl = productThumbnailUrl;
     }
 
     public static InterestProductCreateResponseDto from(InterestProduct interestProduct) {
         return InterestProductCreateResponseDto.builder()
-                                               .productCode(interestProduct.getProductCode())
-                                               .price(interestProduct.getPrice())
-                                               .productThumbnailUrl(interestProduct.getProductThumbnailUrl())
-                                               .build();
+                .productCode(interestProduct.getProductCode())
+                .productName(interestProduct.getProductName())
+                .price(interestProduct.getPrice())
+                .productThumbnailUrl(interestProduct.getProductThumbnailUrl())
+                .build();
     }
 
     public InterestProductCreateResponseVo toVo() {
         return InterestProductCreateResponseVo.builder()
-                                              .productCode(productCode)
-                                              .price(price)
-                                              .productThumbnailUrl(productThumbnailUrl)
-                                              .build();
+                .productCode(productCode)
+                .productName(productName)
+                .price(price)
+                .productThumbnailUrl(productThumbnailUrl)
+                .build();
     }
 }
