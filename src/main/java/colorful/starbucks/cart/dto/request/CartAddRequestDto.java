cba9c139ce7c54c474f192f33f96fc00d7fdf751
woneveryday <woneveryday@naver.com>
2025-03-27 10:59:30 +0900

    :hammer: refactor: Noargs 제거

--- src/main/java/colorful/starbucks/cart/dto/request/CartAddRequestDto.java
+++ src/main/java/colorful/starbucks/cart/dto/request/CartAddRequestDto.java
@@ -4,13 +4,11 @@ import colorful.starbucks.cart.domain.Cart;
 import colorful.starbucks.cart.vo.request.CartAddRequestVo;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
-@NoArgsConstructor
 public class CartAddRequestDto {
 
     private String productDetailCode;
