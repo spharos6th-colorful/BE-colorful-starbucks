b52e9a8cdcc6ae46308a203e89ed9ab29a1df8a0
yryryr96 <s9285s@naver.com>
2025-03-28 17:50:25 +0900

    :sparkles:feat: 이미 등록된 상세 상품일 경우 등록할 수 없도록 수정

--- src/main/java/colorful/starbucks/product/infrastructure/ProductDetailRepository.java
+++ src/main/java/colorful/starbucks/product/infrastructure/ProductDetailRepository.java
@@ -5,10 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductDetailRepository extends JpaRepository<ProductDetail, Long> {
 
     @Query(value = "select pd from ProductDetail pd " +
             "where pd.productCode = :productCode and pd.isDeleted = false")
     List<ProductDetail> findAllByProductCode(String productCode);
+
+    Optional<ProductDetail> findByProductCodeAndSizeIdAndColorId(String productCode, int sizeId, int colorId);
+
+    boolean existsByProductCodeAndSizeIdAndColorIdAndIsDeletedFalse(
+            String productCode, Long sizeId, Long colorId
+    );
 }
