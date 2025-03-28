53505e4b2678dbd5da445bdc638f0e3ad0965928
yryryr96 <s9285s@naver.com>
2025-03-28 10:54:23 +0900

    :sparkles:feat: productCode로 상품을 조회하여 Optional 타입으로 반환

--- src/main/java/colorful/starbucks/product/infrastructure/ProductRepository.java
+++ src/main/java/colorful/starbucks/product/infrastructure/ProductRepository.java
@@ -3,5 +3,8 @@ package colorful.starbucks.product.infrastructure;
 import colorful.starbucks.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductCode(String productCode);
 }
