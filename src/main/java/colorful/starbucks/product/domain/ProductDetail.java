96f918d2c70fc7f232fc22240ba085783f75b304
yryryr96 <s9285s@naver.com>
2025-03-28 11:10:01 +0900

    :sparkles:feat: ProductDetail 엔티티에 Comment 및 Column 제약조건 설정, discountPrice 필드 추가

--- src/main/java/colorful/starbucks/product/domain/ProductDetail.java
+++ src/main/java/colorful/starbucks/product/domain/ProductDetail.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter
@@ -17,32 +18,45 @@ public class ProductDetail extends BaseEntity {
     @Column(name = "product_detail_id")
     private Long id;
 
+    @Comment("상품 상세 코드")
     @Column(nullable = false, unique = true)
     private String productDetailCode;
 
+    @Comment("상품 코드")
     @Column(nullable = false)
     private String productCode;
 
+    @Comment("색상 ID")
     @Column(nullable = false)
     private Long sizeId;
 
+    @Comment("사이즈 이름")
     @Column(nullable = false)
     private String sizeName;
 
+    @Comment("색상 ID")
     @Column(nullable = false)
     private Long colorId;
 
+    @Comment("색상 이름")
     @Column(nullable = false)
     private String colorName;
 
+    @Comment("재고 수량")
     @Column(nullable = false)
     private int inventoryQuantity;
 
+    @Comment("가격")
     @Column(nullable = false)
     private int price;
 
+    @Comment("상품 상세 썸네일 URL")
     @Column(nullable = false)
     private String productDetailThumbnailUrl;
+
+    @Comment("할인 가격")
+    @Column(nullable = false)
+    private int discountPrice;
 
     @Builder
     private ProductDetail(Long id,
