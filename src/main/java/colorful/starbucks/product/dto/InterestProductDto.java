42a46c095176ef2edad5fbe4ab2a73f3d97333b1
정영록 <s9285s@naver.com>
2025-03-30 01:59:18 +0900

    :package:chore: InterestProductDto 클래스 위치 이동

---
+++ src/main/java/colorful/starbucks/product/dto/InterestProductDto.java
@@ -0,0 +1,30 @@
+package colorful.starbucks.product.dto;
+
+import colorful.starbucks.product.domain.InterestProduct;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class InterestProductDto {
+    private String productCode;
+    private String productName;
+    private int price;
+    private String productThumbnailUrl;
+
+    @Builder
+    private InterestProductDto(String productCode, String productName, int price, String productThumbnailUrl) {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.price = price;
+        this.productThumbnailUrl = productThumbnailUrl;
+    }
+
+    public static InterestProductDto from(InterestProduct interestProduct) {
+        return InterestProductDto.builder()
+                .productCode(interestProduct.getProductCode())
+                .productName(interestProduct.getProductName())
+                .price(interestProduct.getPrice())
+                .productThumbnailUrl(interestProduct.getProductThumbnailUrl())
+                .build();
+    }
+}
