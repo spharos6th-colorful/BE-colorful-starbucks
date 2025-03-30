031dd59dde4ba0f616422682e92c694338f53816
woneveryday <woneveryday@naver.com>
2025-03-30 15:25:30 +0900

    :hammer: refactor: delete, create 변경

--- src/main/java/colorful/starbucks/cart/dto/request/CartProductOptionEditRequestDto.java
+++ src/main/java/colorful/starbucks/cart/dto/request/CartProductOptionEditRequestDto.java
@@ -2,6 +2,7 @@ package colorful.starbucks.cart.dto.request;
 
 import colorful.starbucks.cart.domain.Cart;
 import colorful.starbucks.cart.vo.request.CartProductOptionEditRequestVo;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
--- src/main/java/colorful/starbucks/cart/presentation/CartController.java
+++ src/main/java/colorful/starbucks/cart/presentation/CartController.java
@@ -58,7 +58,7 @@ public class CartController {
         );
     }
 
-    @PutMapping("/{memberUuid}/{cartId}")
+    @PatchMapping("/{memberUuid}/{cartId}")
     public ApiResponse<Void> editCartProductOptions(@PathVariable String memberUuid,
                                                                   @PathVariable Long cartId,
                                                                   @RequestBody CartProductOptionEditRequestVo cartProductOptionEditRequestVo){
