da3f5df0e0f021c1ef7ba18d2047e18388adff6c
정영록 <s9285s@naver.com>
2025-04-02 23:03:45 +0900

    :sparkles:feat: 상품-카테고리-리스트 조회 메서드 생성

--- src/main/java/colorful/starbucks/admin/application/ProductCategoryListService.java
+++ src/main/java/colorful/starbucks/admin/application/ProductCategoryListService.java
@@ -1,8 +1,13 @@
 package colorful.starbucks.admin.application;
 
+import colorful.starbucks.admin.dto.ProductCategoryListFilterDto;
 import colorful.starbucks.admin.dto.request.ProductCategoryListCreateRequestDto;
+import colorful.starbucks.admin.dto.response.ProductCategoryCursorResponseDto;
+import colorful.starbucks.common.util.CursorPage;
 
 public interface ProductCategoryListService {
 
     void createProductCategoryList(ProductCategoryListCreateRequestDto productCategoryListCreateRequestDto);
+
+    CursorPage<ProductCategoryCursorResponseDto> getFilteredProductList(ProductCategoryListFilterDto productCategoryListFilterDto);
 }
--- src/main/java/colorful/starbucks/admin/application/ProductCategoryListServiceImpl.java
+++ src/main/java/colorful/starbucks/admin/application/ProductCategoryListServiceImpl.java
@@ -1,7 +1,13 @@
 package colorful.starbucks.admin.application;
 
+import colorful.starbucks.admin.domain.ProductCategoryList;
+import colorful.starbucks.admin.dto.ProductCategoryListFilterDto;
 import colorful.starbucks.admin.dto.request.ProductCategoryListCreateRequestDto;
+import colorful.starbucks.admin.dto.response.ProductCategoryCursorResponseDto;
 import colorful.starbucks.admin.infrastructure.ProductCategoryListRepository;
+import colorful.starbucks.common.exception.BaseException;
+import colorful.starbucks.common.response.ResponseStatus;
+import colorful.starbucks.common.util.CursorPage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,5 +26,24 @@ public class ProductCategoryListServiceImpl implements ProductCategoryListServic
         productCategoryListRepository.save(
                 productCategoryListCreateRequestDto.toEntity()
         );
+    }
+
+    @Override
+    public CursorPage<ProductCategoryCursorResponseDto> getFilteredProductList(ProductCategoryListFilterDto productCategoryListFilterDto) {
+
+        Long id;
+        int price;
+
+        if (productCategoryListFilterDto.getCursor() == null) {
+            id = productCategoryListFilterDto.getSortBy().equals("createdAt,asc") ? 0L : Long.MAX_VALUE;
+            price = productCategoryListFilterDto.getSortBy().equals("price,asc") ? 0 : Integer.MAX_VALUE;
+        } else {
+            ProductCategoryList productCategoryList = productCategoryListRepository.findById(productCategoryListFilterDto.getCursor())
+                    .orElseThrow(() -> new BaseException(ResponseStatus.RESOURCE_NOT_FOUND));
+            id = productCategoryList.getId();
+            price = productCategoryList.getPrice();
+        }
+
+        return productCategoryListRepository.getFilteredProductList(productCategoryListFilterDto, id, price);
     }
 }
