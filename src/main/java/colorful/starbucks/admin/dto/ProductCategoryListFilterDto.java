49b53676ca607e7363270a772f2a79b211a76a21
정영록 <s9285s@naver.com>
2025-04-02 22:57:52 +0900

    :sparkles:feat: 상품-카테고리-리스트 조회 시 필터링 조건 받는 vo, dto 생성

---
+++ src/main/java/colorful/starbucks/admin/dto/ProductCategoryListFilterDto.java
@@ -0,0 +1,48 @@
+package colorful.starbucks.admin.dto;
+
+import colorful.starbucks.admin.vo.ProductCategoryListFilterVo;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ProductCategoryListFilterDto {
+
+    private Long cursor;
+    private Integer minPrice;
+    private Integer maxPrice;
+    private Long topCategoryId;
+    private List<Long> bottomCategoryIds;
+    private Integer size;
+    private String sortBy;
+
+    @Builder
+    private ProductCategoryListFilterDto(Long cursor,
+                                         Integer minPrice,
+                                         Integer maxPrice,
+                                         Long topCategoryId,
+                                         List<Long> bottomCategoryIds,
+                                         Integer size,
+                                         String sortBy) {
+        this.cursor = cursor;
+        this.minPrice = minPrice;
+        this.maxPrice = maxPrice;
+        this.topCategoryId = topCategoryId;
+        this.bottomCategoryIds = bottomCategoryIds;
+        this.size = size;
+        this.sortBy = sortBy;
+    }
+
+    public static ProductCategoryListFilterDto from(ProductCategoryListFilterVo productCategoryListFilterVo) {
+        return ProductCategoryListFilterDto.builder()
+                .cursor(productCategoryListFilterVo.getCursor())
+                .minPrice(productCategoryListFilterVo.getMinPrice())
+                .maxPrice(productCategoryListFilterVo.getMaxPrice())
+                .topCategoryId(productCategoryListFilterVo.getTopCategoryId())
+                .bottomCategoryIds(productCategoryListFilterVo.getBottomCategoryIds())
+                .size(productCategoryListFilterVo.getSize())
+                .sortBy(productCategoryListFilterVo.getSortBy())
+                .build();
+    }
+}
