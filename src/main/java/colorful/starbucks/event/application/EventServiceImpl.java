6e4f1dd4c62dd6b89349d7d0521285fb71e40b39
정영록 <s9285s@naver.com>
2025-04-03 21:02:48 +0900

    :hammer:refactor: @Transactional 위치 이동

--- src/main/java/colorful/starbucks/event/application/EventServiceImpl.java
+++ src/main/java/colorful/starbucks/event/application/EventServiceImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class EventServiceImpl implements EventService {
 
     private final EventRepository eventRepository;
