1835e2a1734c20956072add673522eee0f33c4db
정영록 <s9285s@naver.com>
2025-04-07 23:43:30 +0900

    :sparkles:feat: 회원 쿠폰 발급 기능을 redisson 분산락을 사용해 구현

--- src/main/java/colorful/starbucks/coupon/infrastructure/CouponRepository.java
+++ src/main/java/colorful/starbucks/coupon/infrastructure/CouponRepository.java
@@ -1,11 +1,13 @@
 package colorful.starbucks.coupon.infrastructure;
 
 import colorful.starbucks.coupon.domain.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
-     Optional<Coupon> findByCouponUuid(String couponUuid);
+    Optional<Coupon> findByCouponUuid(String couponUuid);
 }
