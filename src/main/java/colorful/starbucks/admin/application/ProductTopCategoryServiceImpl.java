7b08c3f2619ba50ce64894869373a59046d97574
yryryr96 <s9285s@naver.com>
2025-04-12 02:41:02 +0900

    :hammer:refactor: 상위 카테고리 조회 페이징 제거

--- src/main/java/colorful/starbucks/admin/application/ProductTopCategoryServiceImpl.java
+++ src/main/java/colorful/starbucks/admin/application/ProductTopCategoryServiceImpl.java
@@ -4,7 +4,6 @@ import colorful.starbucks.admin.dto.ProductTopCategoryDtos;
 import colorful.starbucks.admin.dto.request.ProductTopCategoryCreateRequestDto;
 import colorful.starbucks.admin.infrastructure.ProductTopCategoryRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -25,7 +24,7 @@ public class ProductTopCategoryServiceImpl implements ProductTopCategoryService 
     }
 
     @Override
-    public ProductTopCategoryDtos getTopCategories(Pageable pageable) {
-        return ProductTopCategoryDtos.from(productTopCategoryRepository.findAll(pageable));
+    public ProductTopCategoryDtos getTopCategories() {
+        return ProductTopCategoryDtos.from(productTopCategoryRepository.findAll());
     }
 }
