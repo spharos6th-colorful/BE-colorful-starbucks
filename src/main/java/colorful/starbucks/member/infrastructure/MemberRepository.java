2b3c106f16eb25bbe716d67d65fe3f1ffa524d51
woneveryday <woneveryday@naver.com>
2025-04-12 23:30:42 +0900

    :hammer: refactor: 필요없는 repository 메서드 제거

--- src/main/java/colorful/starbucks/member/infrastructure/MemberRepository.java
+++ src/main/java/colorful/starbucks/member/infrastructure/MemberRepository.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findAllByMemberUuid(String memberUuid);
-
     Optional<Member> findByEmailAndMemberNameAndPhoneNumber(String email, String memberName, String phoneNumber);
 
     Optional<Member> findByMemberNameAndPhoneNumber(String memberName, String phoneNumber);
