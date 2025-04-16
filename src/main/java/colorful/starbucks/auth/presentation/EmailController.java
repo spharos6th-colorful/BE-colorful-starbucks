4ffd0e334befe049173f87110c8e9ae7a0a7f57b
정영록 <s9285s@naver.com>
2025-04-16 21:32:08 +0900

    :sparkles:feat: Auth 서비스 스웨거 설명 작성

--- src/main/java/colorful/starbucks/auth/presentation/EmailController.java
+++ src/main/java/colorful/starbucks/auth/presentation/EmailController.java
@@ -7,6 +7,7 @@ import colorful.starbucks.auth.vo.request.EmailCodeSendRequestVo;
 import colorful.starbucks.auth.vo.request.EmailVerifyCodeRequestVo;
 import colorful.starbucks.auth.vo.response.EmailCodeSendResponseVo;
 import colorful.starbucks.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +21,11 @@ public class EmailController {
 
     private final EmailService emailService;
 
+    @Operation(
+            summary = "인증 번호 전송 API",
+            description = "인증 번호를 이메일로 전송하는 API 입니다.",
+            tags = {"AUTH-SERVICE"}
+    )
     @PostMapping("/email/send-code")
     public ApiResponse<EmailCodeSendResponseVo> sendEmail(@RequestBody EmailCodeSendRequestVo emailCodeSendRequestVo) {
         return ApiResponse.ok(
@@ -28,6 +34,11 @@ public class EmailController {
         );
     }
 
+    @Operation(
+            summary = "이메일 인증 확인 API",
+            description = "이메일 인증 번호를 확인하는 API 입니다.",
+            tags = {"AUTH-SERVICE"}
+    )
     @PostMapping("/email/verify-code")
     public ApiResponse<String> verifyEmailCode(@RequestBody EmailVerifyCodeRequestVo emailVerifyCodeRequestVo) {
         emailService.verifyEmailCode(EmailVerifyCodeRequestDto.from(emailVerifyCodeRequestVo));
