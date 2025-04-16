8618cca84ddcb8d9ce8722d9b39e1ca9cd3cdce9
yryryr96 <s9285s@naver.com>
2025-04-16 17:56:13 +0900

    :sparkles:feat: 스웨거 설명 등록

--- src/main/java/colorful/starbucks/product/presentation/ProductController.java
+++ src/main/java/colorful/starbucks/product/presentation/ProductController.java
@@ -12,6 +12,7 @@ import colorful.starbucks.product.vo.response.ProductCursorResponseVo;
 import colorful.starbucks.product.vo.response.ProductOptionListResponseVo;
 import colorful.starbucks.product.vo.response.ProductResponseVo;
 import colorful.starbucks.product.vo.response.ProductSimpleResponseVo;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -25,6 +26,9 @@ public class ProductController {
     private final ProductService productService;
 
     // todo: 프론트에서 s3 업로드 구현 완료되면 file이 아닌 image url을 받는 것으로 수정할 것
+    @Operation(summary = "상품 등록 API",
+            description = "상품 등록 API 입니다.",
+            tags = {"PRODUCT-SERVICE"})
     @PostMapping
     public ApiResponse<ProductResponseVo> createProduct(@RequestPart ProductCreateRequestVo productCreateRequestVo,
                                                         @RequestPart(required = false) MultipartFile productThumbnail,
@@ -37,6 +41,9 @@ public class ProductController {
         );
     }
 
+    @Operation(summary = "상품 목록 조회 API",
+            description = "필터링, 정렬 조건을 포함한 상품 조회 API 입니다.",
+            tags = {"PRODUCT-SERVICE"})
     @GetMapping
     public ApiResponse<CursorPage<ProductCursorResponseVo>> getProductsByFilter(@ModelAttribute ProductFilterVo productFilterVo) {
         return ApiResponse.ok("상품 목록 조회를 완료했습니다.",
@@ -45,6 +52,9 @@ public class ProductController {
         );
     }
 
+    @Operation(summary = "상품 조회 API",
+            description = "상품 코드로 상품에 대한 정보를 조회하는 API 입니다.",
+            tags = {"PRODUCT-SERVICE"})
     @GetMapping("/{productCode}")
     public ApiResponse<ProductResponseVo> getProduct(@PathVariable Long productCode) {
         return ApiResponse.ok(
@@ -53,6 +63,9 @@ public class ProductController {
         );
     }
 
+    @Operation(summary = "상품 옵션 조회 API",
+            description = "상품 코드로 상품 옵션을 조회하는 API 입니다.",
+            tags = {"PRODUCT-SERVICE"})
     @GetMapping("/{productCode}/options")
     public ApiResponse<ProductOptionListResponseVo> getProductOptionList(@PathVariable Long productCode) {
         return ApiResponse.ok("상세 상품 옵션이 조회되었습니다.",
@@ -60,6 +73,9 @@ public class ProductController {
         );
     }
 
+    @Operation(summary = "상품 간단 조회 API",
+            description = "상품 코드로 상품 간단 정보를 조회하는 API 입니다.",
+            tags = {"PRODUCT-SERVICE"})
     @GetMapping("/{productCode}/simple")
     public ApiResponse<ProductSimpleResponseVo> getProductSimpleInformation(@PathVariable Long productCode) {
         return ApiResponse.ok(
--- src/main/java/colorful/starbucks/product/presentation/ProductDetailController.java
+++ src/main/java/colorful/starbucks/product/presentation/ProductDetailController.java
@@ -8,6 +8,7 @@ import colorful.starbucks.product.vo.request.ProductDetailCodeAndQuantityRequest
 import colorful.starbucks.product.vo.request.ProductDetailCreateRequestVo;
 import colorful.starbucks.product.vo.response.ProductDetailCodeAndQuantityResponseVo;
 import colorful.starbucks.product.vo.response.ProductDetailResponseVo;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,6 +20,9 @@ public class ProductDetailController {
 
     private final ProductDetailService productDetailService;
 
+    @Operation(summary = "상세 상품 등록 API",
+            description = "상품 코드, 옵션, 수량 등 다양한 데이터로 상세 상품을 등록하는 API 입니다.",
+            tags = {"PRODUCT-DETAIL-SERVICE"})
     @PostMapping
     public ApiResponse<ProductDetailResponseVo> createProductDetail(@RequestPart ProductDetailCreateRequestVo productDetailCreateRequestVo,
                                                                     @RequestPart MultipartFile productDetailThumbnail) {
@@ -30,6 +34,9 @@ public class ProductDetailController {
         );
     }
 
+    @Operation(summary = "상세 상품 조회 API",
+            description = "상세 상품 코드로 상세 상품에 대한 정보를 조회하는 API 입니다.",
+            tags = {"PRODUCT-DETAIL-SERVICE"})
     @GetMapping("/{productDetailCode}")
     public ApiResponse<ProductDetailResponseVo> getProductDetail(@PathVariable Long productDetailCode) {
         return ApiResponse.ok("상세 상품이 조회되었습니다.",
@@ -37,6 +44,9 @@ public class ProductDetailController {
         );
     }
 
+    @Operation(summary = "상세 상품 코드, 수량 조회 API",
+            description = "상품 코드, 옵션으로 상세 상품 코드와 수량을 조회하는 API 입니다.",
+            tags = {"PRODUCT-DETAIL-SERVICE"})
     @GetMapping
     public ApiResponse<ProductDetailCodeAndQuantityResponseVo> getProductDetailWithOptions(
             @ModelAttribute ProductDetailCodeAndQuantityRequestVo productDetailCodeAndQuantityRequestVo) {
