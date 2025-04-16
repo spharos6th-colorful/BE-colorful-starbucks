de12b8d4d04e25d77e4eaed24b544e7651bba419
yryryr96 <s9285s@naver.com>
2025-04-16 15:50:31 +0900

    :hammer:refactor: 신상품 도메인, 레포지토리 삭제

--- src/main/java/colorful/starbucks/product/infrastructure/NewProductRepository.java
+++
@@ -1,7 +0,0 @@
-package colorful.starbucks.product.infrastructure;
-
-import colorful.starbucks.product.domain.NewProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewProductRepository extends JpaRepository<NewProduct, Long> {
-}
