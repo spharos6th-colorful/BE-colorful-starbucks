4c90b3af4c0fe6ee20c6ea5c4a3b8ad031b75f66
yryryr96 <s9285s@naver.com>
2025-04-21 14:10:25 +0900

    :sparkles:feat: 상위 카테고리 정렬 역순으로 변경

--- src/main/java/colorful/starbucks/admin/infrastructure/ProductTopCategoryRepository.java
+++ src/main/java/colorful/starbucks/admin/infrastructure/ProductTopCategoryRepository.java
@@ -3,5 +3,8 @@ package colorful.starbucks.admin.infrastructure;
 import colorful.starbucks.admin.domain.ProductTopCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductTopCategoryRepository extends JpaRepository<ProductTopCategory, Long> {
+    List<ProductTopCategory> findAllByOrderByIdDesc();
 }
